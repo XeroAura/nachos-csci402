20497d4917c400bb72d53947b9d4026556feccd3
byroncho <byroncho@usc.edu>
2014-06-24 12:52:47 -0700

    Added new class

---
+++ code/test/testlocks.c
@@ -0,0 +1,33 @@
+/* testlocks.c
+ * A test for the lock functionality. Taken in part from the testSuite used in project 1.
+*/
+
+ #include "syscall.h"
+
+void t1_t1(){
+  Acquire(0);
+  Acquire(0);
+  Release(1);
+  Acquire(1);
+  Release(1);
+  Release(0);
+
+}
+
+void t1_t2(){
+}
+
+int main(){
+	int maxLockCount;
+	Write("making locks...\n", 16, ConsoleOutput);
+	for (maxLockCount = 0; maxLockCount < 2; maxLockCount++){
+		CreateLock(maxLockCount);
+	}
+	t1_t1();
+
+	maxLockCount = 0;
+	Write("erasing locks...\n", 18, ConsoleOutput);
+	for(maxLockCount = 0; maxLockCount < 2; maxLockCount++){
+		DestroyLock(maxLockCount);
+    }
+}
